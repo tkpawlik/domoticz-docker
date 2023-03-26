d749ff293e1653d2f8e71f41f3eec5de44ad7692
Tomasz Pawlik <tomasz.pawlik@ovoo.pl>
2023-03-26 17:10:41 +0200

    v1.8.0

--- docker-build-amd64.sh
+++ docker-build-amd64.sh
@@ -3,17 +3,16 @@
 # ./docker-build-amd64.sh /home/tkpawlik/Dropbox/repository
 
 REPOSITORY=$1
-VERSION=1.7.0
+VERSION=1.8.0
 
-cp $REPOSITORY/domoticz_linux_armv7l.tgz ./
-cp $REPOSITORY/libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb ./
-cp $REPOSITORY/wiringpi_2.50_armhf.deb ./
-cp $REPOSITORY/dht22.bin ./
+mkdir tmp
+cp $REPOSITORY/domoticz_linux_armv7l.tgz tmp/
+cp $REPOSITORY/libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb tmp/
+cp $REPOSITORY/wiringpi_2.50_armhf.deb tmp/
+cp $REPOSITORY/dht22.bin tmp/
+cp $REPOSITORY/BatteryLevel.zip tmp/
 
 docker buildx build --platform linux/arm/v7 -t domoticz:$VERSION .
 docker save domoticz:$VERSION > domoticz-$VERSION.tar
 
-rm -f domoticz_linux_armv7l.tgz
-rm -f libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb
-rm -f dht22.bin
-rm -f wiringpi_2.50_armhf.deb
+rm -rf tmp
