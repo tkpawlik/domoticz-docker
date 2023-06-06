5937b7dcbfda6ce1b4bb1874765d9394bb040380
Tomasz Pawlik <tomasz.pawlik@ovoo.pl>
2023-06-06 12:00:12 +0200

    Add mosquitto

--- docker-run.sh
+++ docker-run.sh
@@ -4,7 +4,24 @@ VERSION=$1
 
 docker stop domoticz
 docker rm -f domoticz
+#docker rm -f ewpe-smart-mqtt
+docker rm -f mosquitto
+
+docker run --name mosquitto \
+  --network host \
+  --restart unless-stopped \
+  -d \
+  eclipse-mosquitto:2.0.1
+
+#docker run --name ewpe-smart-mqtt \
+#  --network host \
+#  --restart unless-stopped \
+#  -e "DEVICE_POLL_INTERVAL=30000" \
+#  -d \
+#  ewpe-smart-mqtt:armv7
+
 docker run --name domoticz \
+  --network host \
   --tmpfs /tmpfs \
   --device=/dev/ttyUSB0 \
   --device=/dev/ttyACM0 \
@@ -13,7 +30,6 @@ docker run --name domoticz \
   -v /mnt/data/domoticz:/data \
   -v /sys:/sys \
   -e TZ=Europe/Warsaw \
-  -p 8080:8080 \
   -d \
   domoticz:$VERSION
 docker ps | grep domoticz
